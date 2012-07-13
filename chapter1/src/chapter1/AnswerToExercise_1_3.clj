3225ede889136ceb31ce0567782f9b53467aff00
Andreas Krüger <Andreas.Krueger@innoQ.com>
2012-07-13 20:43:09 +0200

    Be more consistent about types - though experimentally, this did not change anything. Still get Integers, not Longs.

--- chapter1/src/chapter1/AnswerToExercise_1_3.clj
+++ chapter1/src/chapter1/AnswerToExercise_1_3.clj
@@ -1,8 +1,8 @@
 (ns chapter1.Answers)
 
-(defn sos [x, y] (+ (* x x) (* y y)))
+(defn sos [^Long x, ^Long y] (+ (* x x) (* y y)))
 
-(defn ex1_3 [^Integer a, ^Integer b, ^Integer c]
+(defn ex1_3 [^Long a, ^Long b, ^Long c]
   (cond 
     (and (<= a b) (<= a c)) (sos b c)
     (and (<= b a) (<= b c)) (sos a c)
