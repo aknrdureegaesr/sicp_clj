de8c9fed6c2f46c15f6215e1a0378bc2291dab80
Andreas Krüger <Andreas.Krueger@innoQ.com>
2012-07-16 17:45:45 +0200

    Exhaustive comments.

--- chapter1/src/chapter1/sqrt/ReallyGoodSQRT.clj
+++ chapter1/src/chapter1/sqrt/ReallyGoodSQRT.clj
@@ -1,35 +1,41 @@
 (ns chapter1.BonusMaterial)
 
-; Heron's formula:
-; If approx is an approximation for sqrt(x),
-; then (* 0.5 (+ approx ( / x approx))
-; is an even better approximation for sqrt(x)
-; (for positive x, and positive approx)
+;;; Heron's formula:
+;; If approx is an approximation for sqrt(x),
+;; then (* 0.5 (+ approx ( / x approx))
+;; is an even better approximation for sqrt(x)
+;; (for positive x, and positive approx)
 (defn heron [x approx] (* 0.5 (+ approx ( / x approx))))
 
-; Little known fact:
-; In theory, if one keeps re-applying Heron's formula,
-; the values will become lower and lower.
-; They approach sqrt(x) from above.
-; Only the very first value, the start value,
-; could potentially be below sqrt(x) - all else
-; will be above that.
+;; Little known fact:
+;; Heron's formula has a globel minimum
+;; (among all positive numbers) at the sqrt's value.
+;; This can be easily checked with elementary calculus.
+;; So, if one keeps re-applying Heron's formula,
+;; the values will become lower and lower.
+;; They approach sqrt(x) from above.
+;; Only the very first value, the start value,
+;; could potentially be below sqrt(x) - all others
+;; will be above that.
 
-; But we are in finite-precision arithmetic here.
+;; But we are using finite-precision arithmetic here.
 
-; So the values cannot become lower and lower forever
-; and still stay above sqrt(x).
+;; So the values cannot become lower and lower forever
+;; (and still stay above sqrt(x)).
 
-; When the calculated approximation value remains equal,
-; or rises ever so slightly,
-; compared to the previous approximation value,
-; that is the precise moment when the maximal precision
-; has been reached
-; and further approximations will no longer yield
-; better precision.
-;
-; So this is an excellent criterion when to stop.
-;
+;; When the calculated finite-precision approximation value
+;; remains equal, or rises ever so slightly,
+;; compared to the previous approximation value,
+;; that is the precise moment when the maximal precision
+;; has been reached
+;; and further approximations will no longer yield
+;; better precision.
+
+;; So this is an excellent criterion when to stop.
+
+;; Here is the re-application of Heron's formula
+;; until the values no longer fall:
+
 (defn keep_improving_until_precision_is_reached
   ([x approx better_approx]
     (if (< better_approx approx)
@@ -38,23 +44,36 @@
   ([x approx]
     (keep_improving_until_precision_is_reached x approx (heron x approx))))
 
-; To make this work, one needs a start value
-; that is certainly higher than sqrt(x).
+;; To make this work, one needs a start value
+;; that is certainly higher than sqrt(x).
 
-; For starters, I brutally used something slightly above
-; Math.sqrt(Double.MAX_VALUE)
+;; For starters, I brutally used something slightly above
+;; Math.sqrt(Double.MAX_VALUE)
 
-; (defn upper_start_value [x] 1.35E154)
+;; (defn upper_start_value [x] 1.35E154)
 
-; This gives precise results, but performance is far from ideal.
+;; This gives precise results, but tends to generate
+;; a few hundered iterations before the proximity
+;; of the real value is reached.
 
-; Here is a more complicated implementation.
+;; For values above the root and far away from it,
+;; each application of Heron's formula tends to 
+;; reduce the distance to the root to 50 %.
+;; (To prove this, one needs the mean value theorem
+;; of calculus, nowadays no longer taught at
+;; high school level.)
 
-; First I need a power function.
+;; Once the value is close to the sqrt-value,
+;; each new application approximately doubles
+;; the numbers of correct digits.
 
-; I could have used the one from Math,
-; (defn power [x n] (Math/pow x n))
-; but, for the fun of it, here is the pedestrian way:
+;; Here is a more complicated implementation.
+
+;; First I need a power function.
+
+;; I could have used the one from Math,
+;; (defn power [x n] (Math/pow x n))
+;; but, for the fun of it, here is the pedestrian way:
 
 (defn power [x n]
   (cond
@@ -63,17 +82,18 @@
     (= 1 (rem n 2)) (* x (power (* x x) (quot n 2)))
     true (recur (* x x) (quot n 2))))
 
-; To find some approximate start value
-; that is higher than the true value,
-; first use the bit representation of the number
-; to quickly find some reasonable initial value,
-; then use one iteration of Heron's formula
-; to improve on that initial value
-; and (more importantly here) make sure
-; the improvement is above the true value.
+;; To find some approximate start value
+;; that is higher than the true value,
+;; first use the bit representation of the number
+;; to quickly find some reasonable initial value,
+;; then use just one application of Heron's formula to
+;; a) improve on that initial value and
+;; b) (more importantly here) make sure
+;; the improvement is above the true value.
 (defn upper_start_value [x]
   (heron x (power 1.4142 (Math/getExponent x))))
 
+;; Putting it all together:
 (defn mysqrt [x] (keep_improving_until_precision_is_reached x (upper_start_value x)))
 
 ; Alternatively, call the standard Java implementation
